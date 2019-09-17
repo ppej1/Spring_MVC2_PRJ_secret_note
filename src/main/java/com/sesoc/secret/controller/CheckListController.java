0b10e886487e64cca1c2e515c6a778e81d5c884f
ppej1 <xodnjs1016@gmail.com>
2019-09-17 18:00:58 +0900

    0917 1800

--- src/main/java/com/sesoc/secret/controller/CheckListController.java
+++ src/main/java/com/sesoc/secret/controller/CheckListController.java
@@ -1,8 +1,30 @@
 package com.sesoc.secret.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import global.sesoc.ConsolGame.dto.ConsolGame;
+import global.sesoc.ConsolGame.dto.ConsolGameStatusVO;
 
 @Controller
 public class CheckListController {
+	
+	
+	
+	
+	/*ajax*/
+	@RequestMapping(value = "/loadCheckList", method = RequestMethod.POST)
+	@ResponseBody
+	public ArrayList<CheckListVO> listOfGame(CheckListVO consolGame, String searchList, String searchItem){
+		repo.checkDelayLendForAllLentBook();
+		ArrayList<ConsolGameStatusVO> list = repo.selectAll(consolGame,searchList,searchItem);
 
+		return list;
+	}
+	
 }
--- src/main/webapp/resources/js/calendar.js
+++ src/main/webapp/resources/js/calendar.js
@@ -11,11 +11,13 @@ document.addEventListener('DOMContentLoaded', function () {
 		},
 		eventClick: function (info) {
 			object = info.event;
-			console.log("a");
+			date = new Date(object.start);
+			startDate = dateToYYYYMMDD(date)
+			
 			$('.flipbook').turn("disable", false);
 			$('.flipbook').turn("page", 6);
 			$('.flipbook').turn("disable", true);
-			checklistPage(object.id);
+			checklistPage(object.id, startDate);
 		},
 		defaultDate: '2019-09-15',
 		events: [{
@@ -25,11 +27,13 @@ document.addEventListener('DOMContentLoaded', function () {
 			color: 'red'
 		},
 		{
+			id: '444',
 			title: 'Check List',
 			start: '2019-09-01',
 			color: 'blue',
 		},
 		{
+			id: 33,
 			title: 'Check List',
 			start: '2019-09-02',
 			color: 'red'
@@ -40,7 +44,32 @@ document.addEventListener('DOMContentLoaded', function () {
 	calendar.render();
 });
 
+function dateToYYYYMMDD(date){
+    function pad(num) {
+        num = num + '';
+        return num.length < 2 ? '0' + num : num;
+    }
+    return date.getFullYear() + '-' + pad(date.getMonth()+1) + '-' + pad(date.getDate());
+}
+
+
+
+
+
+
 function checklistPage(){
-	console.log("b");
 	console.log(object.id);
+	console.log(startDate);
+	
+	var sendData = {
+      		"startDate": startDate
+         }
+         $.ajax({
+           type: 'POST',
+           url: 'loadCheckList',
+           data: sendData,
+           success: function(){
+           	output();
+           }
+         });
 }
