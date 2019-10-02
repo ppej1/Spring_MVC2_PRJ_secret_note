a71aa419b1a13839f01c30f1552110261b87ff39
ppej1 <xodnjs1016@gmail.com>
2019-10-02 21:54:13 +0900

    1002 2154

--- src/main/front/App.js
+++ src/main/front/App.js
@@ -2,14 +2,26 @@ import React, {Component} from 'react';
 import Subject from './Subject';
 import ButtonReact from './ButtonReact';
 export default class App extends Component {
-  
+	constructor(props){
+		super(props);
+		this.state={
+				mode:'welcome'
+		}
+	}
 	render() {
-	  
+		var _article, _buttonDiv = null;
+	  if(this.state.mode ==='welcome'){
+		  _article = <Subject></Subject>
+		  _buttonDiv = <ButtonReact></ButtonReact>
+	  }else if(this.state.mode === 'read'){
+		  _article =<h1>dddd</h1>
+		  _buttonDiv = <h1>bbbb</h1>
+	  }
   
     return (　
     	<div class="info_div">
-			<Subject></Subject>
-			<ButtonReact></ButtonReact>
+    			{_article}
+    			{_buttonDiv}
 		</div>	
     );
   }
